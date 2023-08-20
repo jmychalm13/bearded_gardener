1e920c9f699a60d363ceabf1af0a47972935270a
“Jessica <jmychalm13@gmail.com>
2023-08-20 08:51:20 -0500

    state before adding favicon files

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,12 +1,10 @@
 export function Content() {
   return (
     <div id="content" className="container">
-      <h1 className="my-4 text-center">
-        Welcome to the <span className="special-text">Bearded Garden</span>
-      </h1>
+      <h1 className="my-4 text-center">Welcome to the Bearded Garden</h1>
       <h3 className="text-center mb-5">Quality Plants, Quality Service</h3>
       <div className="container-fluid">
-        <div className="images row">
+        <div className="images row gy-4">
           <div className="col-sm col-md-6 col-lg-3 text-center">
             <img src="src/assets/chonk.jpeg" alt="" />
           </div>
